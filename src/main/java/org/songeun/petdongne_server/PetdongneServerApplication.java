5f572aa4641d7c798eba59e9ec187f4d6331bd60
mungsil <thddmd2009@gmail.com>
2025-04-30 19:29:20 +0900

    spring 프로젝트 생성

---
+++ src/main/java/org/songeun/petdongne_server/PetdongneServerApplication.java
@@ -0,0 +1,13 @@
+package org.songeun.petdongne_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdongneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetdongneServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/songeun/petdongne_server/PetdongneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.songeun.petdongne_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetdongneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
