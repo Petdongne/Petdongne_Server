aff3060ed9691bfc43b9c3b275c68cffb6ee11bb
mungsil <thddmd2009@gmail.com>
2025-05-04 19:48:03 +0900

    feat: review 관련 엔티티 작성 #1

---
+++ src/main/java/org/songeun/petdongne_server/domain/review/entity/ReviewPhoto.java
@@ -0,0 +1,36 @@
+package org.songeun.petdongne_server.domain.review.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.songeun.petdongne_server.global.common.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    private String url;
+
+    @NotNull
+    private Boolean petPhoto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_residence_review_id")
+    private PetResidenceReview review;
+
+    @Builder
+    private ReviewPhoto(String url, Boolean petPhoto, PetResidenceReview review) {
+        this.url = url;
+        this.petPhoto = petPhoto;
+        this.review = review;
+    }
+
+}
