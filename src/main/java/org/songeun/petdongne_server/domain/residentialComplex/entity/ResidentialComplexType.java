cba830e9c7f66a8c3cd047c6b5c794cf3eb0f9e1
mungsil <thddmd2009@gmail.com>
2025-05-04 19:47:01 +0900

    feat: ResidentialComplex 엔티티 및 enum converter 작성 #1

---
+++ src/main/java/org/songeun/petdongne_server/domain/residentialComplex/entity/ResidentialComplexType.java
@@ -0,0 +1,28 @@
+package org.songeun.petdongne_server.domain.residentialComplex.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Getter
+public enum ResidentialComplexType {
+
+    OFFICETEL("OFFICETEL", "오피스텔"),
+    APARTMENT("APARTMENT", "아파트"),
+    VILLA("VILLA", "빌라"),
+    STUDIO("STUDIO", "원룸");
+
+    private final String code;
+    private final String description;
+
+    public static ResidentialComplexType fromCode(String code) {
+
+        return Arrays.stream(ResidentialComplexType.values())
+                .filter(t-> t.code.equals(code))
+                .findAny()
+                .orElseThrow(()-> new RuntimeException("에엥"));
+    }
+
+}
