e1301cb79af7f555d94ef99f13406cd31818c68d
mungsil <thddmd2009@gmail.com>
2025-05-04 19:51:11 +0900

    feat: survey 관련 엔티티 작성 #1

---
+++ src/main/java/org/songeun/petdongne_server/domain/survey/entity/SurveyQuestion.java
@@ -0,0 +1,27 @@
+package org.songeun.petdongne_server.domain.survey.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.songeun.petdongne_server.global.common.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SurveyQuestion extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String questionText;
+
+    @Builder
+    private SurveyQuestion(String questionText) {
+        this.questionText = questionText;
+    }
+}
---
+++ src/main/java/org/songeun/petdongne_server/domain/survey/entity/SurveyResponse.java
@@ -0,0 +1,32 @@
+package org.songeun.petdongne_server.domain.survey.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.songeun.petdongne_server.domain.review.entity.PetResidenceReview;
+import org.songeun.petdongne_server.global.common.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SurveyResponse extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_optoin_id")
+    private SurveyOption surveyOption;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_residence_review_id")
+    private PetResidenceReview residenceReview;
+
+    @Builder
+    private SurveyResponse(SurveyOption surveyOption, PetResidenceReview residenceReview) {
+        this.surveyOption = surveyOption;
+        this.residenceReview = residenceReview;
+    }
+
+}
