dbfaa12a89bfc58bf5d964b28a8a186423bc2cd2
mungsil <thddmd2009@gmail.com>
2025-05-05 20:07:14 +0900

    feat: 공통 성공 코드 정의 #1

---
+++ src/main/java/org/songeun/petdongne_server/global/common/SuccessStatus.java
@@ -0,0 +1,22 @@
+package org.songeun.petdongne_server.global.common;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatus {
+
+    /**
+     * COMMON
+     */
+    OK(HttpStatus.OK,"SUCCESS",  "요청이 성공했습니다."),
+    CREATED(HttpStatus.CREATED,"CREATED", "요청이 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
