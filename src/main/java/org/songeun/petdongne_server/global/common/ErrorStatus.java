26326a2160f4e80489eedc503671e7e9e390ba7a
mungsil <thddmd2009@gmail.com>
2025-05-11 17:52:22 +0900

    feat: Error 상태 코드 인터페이스 정의 #5

--- src/main/java/org/songeun/petdongne_server/global/common/ErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/global/common/ErrorStatus.java
@@ -1,28 +1,11 @@
 package org.songeun.petdongne_server.global.common;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public enum ErrorStatus {
+public interface ErrorStatus {
 
-    /**
-     * COMMON
-     */
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 요청입니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "METHOD_NOT_ALLOWED", "허용되지 않는 HTTP 메소드입니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "인증이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN", "금지된 요청입니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND,"NOT_FOUND", "요청하신 리소스를 찾을 수 없습니다."),
-    CONFLICT(HttpStatus.CONFLICT, "CONFLICT", "이미 존재하는 리소스입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버에 오류가 발생했습니다."),
-    ;
-
-    private final HttpStatus httpStatus;
-    private final String code;
-    private final String message;
+    public HttpStatus getHttpStatus();
+    public String getCode();
+    public String getMessage();
 
 }
