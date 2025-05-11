90ce4c8c858dc1b62fece5a08e78a5a6ee81a719
mungsil <thddmd2009@gmail.com>
2025-05-11 19:03:20 +0900

    refactor: review domain 엔티티 패키지 생성 및 이동 #5

--- src/main/java/org/songeun/petdongne_server/review/infrastructure/ResidenceReviewRepository.java
+++ src/main/java/org/songeun/petdongne_server/review/infrastructure/ResidenceReviewRepository.java
@@ -1,6 +1,6 @@
 package org.songeun.petdongne_server.review.infrastructure;
 
-import org.songeun.petdongne_server.review.domain.ResidenceReview;
+import org.songeun.petdongne_server.review.domain.entity.ResidenceReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/org/songeun/petdongne_server/review/infrastructure/ReviewStatRepository.java
+++ src/main/java/org/songeun/petdongne_server/review/infrastructure/ReviewStatRepository.java
@@ -1,6 +1,6 @@
 package org.songeun.petdongne_server.review.infrastructure;
 
-import org.songeun.petdongne_server.review.domain.ReviewStat;
+import org.songeun.petdongne_server.review.domain.entity.ReviewStat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
