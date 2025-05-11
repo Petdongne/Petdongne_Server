ec2610e00b9bee1da4a936d11fd381696f225370
mungsil <thddmd2009@gmail.com>
2025-05-11 19:04:35 +0900

    refactor: survey domain 엔티티 패키지 생성 및 이동 #5

--- src/main/java/org/songeun/petdongne_server/survey/infrastructure/SurveyOptionRepository.java
+++ src/main/java/org/songeun/petdongne_server/survey/infrastructure/SurveyOptionRepository.java
@@ -1,6 +1,6 @@
 package org.songeun.petdongne_server.survey.infrastructure;
 
-import org.songeun.petdongne_server.survey.domain.SurveyOption;
+import org.songeun.petdongne_server.survey.domain.entity.SurveyOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
