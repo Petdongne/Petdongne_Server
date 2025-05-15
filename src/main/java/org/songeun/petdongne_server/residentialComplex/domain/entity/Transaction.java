5958bf7f49c7b3ea474eeda15fb1574ecf5d8a2f
mungsil <thddmd2009@gmail.com>
2025-05-16 00:33:38 +0900

    feat: Transaction 팩토리 메서드에 도메인 제약 조건 작성
    - TransactionType, Area는 null 이면 안된다.
    - 거래 날짜는 미래이면 안된다.
    - 거래 날짜는 주거 단지의 사용 승인일보다 과거이면 안된다.
    - 거래 금액은 0원 이하이면 안된다.
    
     Issues: #3

--- src/main/java/org/songeun/petdongne_server/residentialComplex/domain/entity/Transaction.java
+++ src/main/java/org/songeun/petdongne_server/residentialComplex/domain/entity/Transaction.java
@@ -7,8 +7,12 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.songeun.petdongne_server.global.common.BaseEntity;
 import org.songeun.petdongne_server.global.converter.TransactionTypeConverter;
+import org.songeun.petdongne_server.global.exception.BusinessException;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
+import static org.songeun.petdongne_server.residentialComplex.domain.error.ResidentialComplexErrorStatus.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,7 +26,7 @@ public class Transaction extends BaseEntity {
     private LocalDate date;
 
     @NotNull
-    private Double price;
+    private Long price;
 
     @NotNull
     @Convert(converter = TransactionTypeConverter.class)
@@ -32,7 +36,12 @@ public class Transaction extends BaseEntity {
     @JoinColumn(name = "area_id")
     private Area area;
 
-    public static Transaction of(LocalDate date, Double price, TransactionType type, Area area){
+    public static Transaction of(LocalDate date, Long price, TransactionType type, Area area){
+        ensureNotNull(type);
+        ensureNotNull(area);
+        ensureDateIsBeforeOrEqualNow(date);
+        ensureDateIsEqualOrAfterApproveDate(date, area);
+        ensurePriceIsPositive(price);
 
         return Transaction.builder()
                 .date(date)
@@ -41,8 +50,38 @@ public class Transaction extends BaseEntity {
                 .area(area).build();
     }
 
+    private static void ensureNotNull(TransactionType type) {
+        if(type == null){
+            throw new BusinessException(TRANSACTION_TYPE_REQUIRED);
+        }
+    }
+
+    private static void ensureNotNull(Area area) {
+        if(area == null){
+            throw new BusinessException(AREA_REQUIRED);
+        }
+    }
+
+    private static void ensureDateIsBeforeOrEqualNow(LocalDate date) {
+        if(date.isAfter(LocalDate.now())) {
+            throw new BusinessException(TRANSACTION_DATE_IS_AFTER_TODAY);
+        }
+    }
+
+    private static void ensureDateIsEqualOrAfterApproveDate(LocalDate date, Area area) {
+        if (date.isBefore(area.getResidentialComplexApprovalDate())) {
+            throw new BusinessException(TRANSACTION_DATE_BEFORE_APPROVE_DATE);
+        }
+    }
+
+    private static void ensurePriceIsPositive(Long price) {
+        if(price <= 0){
+            throw new BusinessException(PRICE_MUST_BE_POSITIVE);
+        }
+    }
+
     @Builder
-    private Transaction(LocalDate date, Double price, TransactionType type, Area area) {
+    private Transaction(LocalDate date, Long price, TransactionType type, Area area) {
         this.date = date;
         this.price = price;
         this.type = type;
--- src/main/java/org/songeun/petdongne_server/residentialComplex/domain/error/ResidentialComplexErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/residentialComplex/domain/error/ResidentialComplexErrorStatus.java
@@ -11,7 +11,16 @@ import org.springframework.http.HttpStatus;
 public enum ResidentialComplexErrorStatus implements ErrorStatus {
 
     RESIDENTIAL_COMPLEX_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RESIDENTIAL_COMPLEX_TYPE_NOT_FOUND", "존재하지 않는 주거 단지 유형입니다."),
+
     TRANSACTION_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "TRANSACTION_TYPE_NOT_FOUND", "존재하지 않는 거래 유형입니다."),
+    TRANSACTION_TYPE_REQUIRED(HttpStatus.BAD_REQUEST, "TRANSACTION_TYPE_REQUIRED", "거래 유형은 NULL일 수 없습니다"),
+
+    TRANSACTION_DATE_IS_AFTER_TODAY(HttpStatus.BAD_REQUEST, "TRANSACTION_DATE_EXCEED_NOW", "거래 날짜는 과거 또는 현재여야 합니다."),
+    TRANSACTION_DATE_BEFORE_APPROVE_DATE(HttpStatus.BAD_REQUEST, "TRANSACTION_DATE_BEFORE_APPROVE_DATE", "거래 날짜는 사용 승인일자와 같거나 이전이어야 합니다."),
+
+    AREA_REQUIRED(HttpStatus.BAD_REQUEST, "AREA_REQUIRED", "주거 면적은 NULL일 수 없습니다."),
+    PRICE_MUST_BE_POSITIVE(HttpStatus.BAD_REQUEST, "PRICE_MUST_BE_POSITIVE", "거래 가격은 0원을 초과해야 합니다."),
+
     ;
 
     private final HttpStatus httpStatus;
