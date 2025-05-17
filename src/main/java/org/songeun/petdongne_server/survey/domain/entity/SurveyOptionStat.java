0f0778dc36731f7893b4d8193ed0e1eb98a33d05
mungsil <thddmd2009@gmail.com>
2025-05-17 21:07:51 +0900

    feat: SurveyOptionStat 팩토리 메서드에 도메인 제약 조건 작성 #3

--- src/main/java/org/songeun/petdongne_server/survey/domain/entity/SurveyOptionStat.java
+++ src/main/java/org/songeun/petdongne_server/survey/domain/entity/SurveyOptionStat.java
@@ -3,10 +3,15 @@ package org.songeun.petdongne_server.survey.domain.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.songeun.petdongne_server.global.exception.BusinessException;
 import org.songeun.petdongne_server.residentialComplex.domain.entity.ResidentialComplex;
 import org.songeun.petdongne_server.global.common.BaseEntity;
+import org.songeun.petdongne_server.survey.domain.error.SurveyErrorStatus;
+
+import static org.songeun.petdongne_server.survey.domain.error.SurveyErrorStatus.SURVEY_OPTION_STAT_SELECTED_COUNT_INVALID;
 
 @Entity
 @Table(
@@ -33,7 +38,16 @@ public class SurveyOptionStat extends BaseEntity {
     @JoinColumn(name = "survey_option_id")
     private SurveyOption surveyOption;
 
+    public Integer selectedCount() {
+        return this.selectedCount;
+    }
+
     public static SurveyOptionStat of(Integer selectedCount, ResidentialComplex residentialComplex, SurveyOption surveyOption){
+        ensureNotNull(selectedCount, NotNullField.SELECTED_COUNT);
+        ensureNotNull(residentialComplex, NotNullField.RESIDENTIAL_COMPLEX);
+        ensureNotNull(surveyOption, NotNullField.SURVEY_OPTION);
+
+        ensureSelectedCountIsValid(selectedCount);
 
         return SurveyOptionStat.builder()
                 .selectedCount(selectedCount)
@@ -41,11 +55,33 @@ public class SurveyOptionStat extends BaseEntity {
                 .surveyOption(surveyOption).build();
     }
 
+    private static <T> void ensureNotNull(T value, NotNullField notNullField) {
+        if (value == null) {
+            throw new BusinessException(notNullField.errorStatus);
+        }
+    }
+
+    private static void ensureSelectedCountIsValid(Integer selectedCount) {
+        if (selectedCount < 0) {
+            throw new BusinessException(SURVEY_OPTION_STAT_SELECTED_COUNT_INVALID);
+        }
+    }
+
     @Builder
     private SurveyOptionStat(Integer selectedCount, ResidentialComplex residentialComplex, SurveyOption surveyOption) {
         this.selectedCount = selectedCount == null ? 0 : selectedCount;
         this.residentialComplex = residentialComplex;
         this.surveyOption = surveyOption;
+    }
+
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    private enum NotNullField {
+        SELECTED_COUNT("선택 횟수", SurveyErrorStatus.SURVEY_OPTION_STAT_SELECTED_COUNT_REQUIRED),
+        RESIDENTIAL_COMPLEX("주거 단지", SurveyErrorStatus.SURVEY_OPTION_STAT_RESIDENTIAL_COMPLEX_REQUIRED),
+        SURVEY_OPTION("설문 옵션", SurveyErrorStatus.SURVEY_OPTION_REQUIRED);
+
+        private final String description;
+        private final SurveyErrorStatus errorStatus;
     }
 
 }
