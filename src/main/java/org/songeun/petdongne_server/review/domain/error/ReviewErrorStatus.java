35048f571c3829c5b7654253e44915e2542ba5e3
mungsil <thddmd2009@gmail.com>
2025-05-17 17:34:43 +0900

    feat: 반려동물 사진 여부 필드명 변경, Review Photo 팩토리 메서드에 도메인 제약 조건 작성 #3

--- src/main/java/org/songeun/petdongne_server/review/domain/error/ReviewErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/review/domain/error/ReviewErrorStatus.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum ReviewErrorStatus implements ErrorStatus {
     REVIEW_TEXT_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_TEXT_REQUIRED", "리뷰 내용은 NULL 또는 공백일 수 없습니다."),
     REVIEW_TEXT_INVALID_LENGTH(HttpStatus.BAD_REQUEST, "REVIEW_TEXT_INVALID_LENGTH", "리뷰 내용은 50자 이상 1000자 이하여야 합니다."),
+
+    REVIEW_RATING_RANGE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW_RATING_RANGE_INVALID", "평점은 0.0보다 크고 5.0 이하인 값만 가질 수 있습니다."),
     REVIEW_RATING_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_RATING_REQUIRED", "리뷰 평점은 NULL일 수 없습니다."),
 
     REVIEW_USER_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_USER_REQUIRED", "리뷰 작성자는 NULL일 수 없습니다."),
@@ -19,7 +21,10 @@ public enum ReviewErrorStatus implements ErrorStatus {
     LAST_OCCUPIED_YEAR_REQUIRED(HttpStatus.BAD_REQUEST, "LAST_OCCUPIED_YEAR_REQUIRED", "거주 연도는 NULL일 수 없습니다."),
     LAST_OCCUPIED_YEAR_INVALID(HttpStatus.BAD_REQUEST, "LAST_OCCUPIED_YEAR_INVALID", "거주 연도는 거주한 단지의 사용 승인일의 연도와 같거나 그 이후여야 합니다."),
 
-    REVIEW_RATING_RANGE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW_RATING_RANGE_INVALID", "평점은 0.0보다 크고 5.0 이하인 값만 가질 수 있습니다.");
+    REVIEW_PHOTO_URL_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_PHOTO_URL_REQUIRED", "리뷰 사진의 URL은 NULL일 수 없습니다."),
+    REVIEW_PHOTO_TYPE_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_PHOTO_TYPE_REQUIRED", "반려동물 여부는 NULL일 수 없습니다"),
+
+    REVIEW_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_REQUIRED", "리뷰는 NULL일 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
