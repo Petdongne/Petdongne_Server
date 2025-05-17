692ddec43f29abd3f5a4f3f72fbbc3ba79bc6948
mungsil <thddmd2009@gmail.com>
2025-05-17 20:43:27 +0900

    feat: SurveyOption 팩토리 메서드에 도메인 제약 조건 작성 #3

--- src/main/java/org/songeun/petdongne_server/survey/domain/error/SurveyErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/survey/domain/error/SurveyErrorStatus.java
@@ -13,6 +13,11 @@ public enum SurveyErrorStatus implements ErrorStatus {
     SURVEY_QUESTION_TEXT_EMPTY(HttpStatus.BAD_REQUEST, "SURVEY_QUESTION_TEXT_EMPTY", "질문 내용은 공백일 수 없습니다."),
     SURVEY_QUESTION_TEXT_TOO_LONG(HttpStatus.BAD_REQUEST, "SURVEY_QUESTION_TEXT_TOO_LONG", "질문 내용은 255자 이하여야 합니다."),
 
+    SURVEY_OPTION_TEXT_REQUIRED(HttpStatus.BAD_REQUEST, "SURVEY_OPTION_TEXT_REQUIRED", "옵션 내용은 NULL일 수 없습니다."),
+    SURVEY_OPTION_TEXT_EMPTY(HttpStatus.BAD_REQUEST, "SURVEY_OPTION_TEXT_EMPTY", "옵션 내용은 공백일 수 없습니다."),
+    SURVEY_OPTION_TEXT_TOO_LONG(HttpStatus.BAD_REQUEST, "SURVEY_OPTION_TEXT_TOO_LONG", "옵션 내용은 150자 이하여야 합니다."),
+
+    SURVEY_QUESTION_REQUIRED(HttpStatus.BAD_REQUEST, "SURVEY_QUESTION_REQUIRED", "설문 질문은 NULL일 수 없습니다.");
     ;
 
     private final HttpStatus httpStatus;
