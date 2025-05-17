08d64cc7a4281ccdfb9f11a6ee9d9250fc388967
mungsil <thddmd2009@gmail.com>
2025-05-17 11:52:35 +0900

    feat: PetFacility 팩토리 메서드에 도메인 제약 조건 작성 #3

--- src/main/java/org/songeun/petdongne_server/facility/domain/entity/PetFacility.java
+++ src/main/java/org/songeun/petdongne_server/facility/domain/entity/PetFacility.java
@@ -3,12 +3,17 @@ package org.songeun.petdongne_server.facility.domain.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.geolatte.geom.G2D;
 import org.geolatte.geom.Point;
+import org.songeun.petdongne_server.facility.domain.error.FacilityErrorStatus;
 import org.songeun.petdongne_server.global.common.BaseEntity;
 import org.songeun.petdongne_server.global.converter.FacilityTypeConverter;
+import org.songeun.petdongne_server.global.exception.BusinessException;
+
+import static org.songeun.petdongne_server.facility.domain.error.FacilityErrorStatus.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,6 +34,11 @@ public class PetFacility extends BaseEntity {
     private Point<G2D> location;
 
     public static PetFacility of(Point<G2D> location, FacilityType type, String name){
+        ensureNotNull(location, NotNullField.LOCATION);
+        ensureNotNull(type, NotNullField.TYPE);
+        ensureNotNull(name, NotNullField.NAME);
+
+        ensureNameIsValid(name);
 
         return PetFacility.builder()
                 .location(location)
@@ -36,11 +46,36 @@ public class PetFacility extends BaseEntity {
                 .name(name).build();
     }
 
+
+    private static <T> void ensureNotNull(T value, NotNullField notNullField) {
+        if (value == null) {
+            throw new BusinessException(notNullField.errorStatus);
+        }
+    }
+
+    private static void ensureNameIsValid(String name) {
+        if (name.trim().isEmpty()) {
+            throw new BusinessException(FACILITY_NAME_REQUIRED);
+        }
+    }
+
     @Builder
     private PetFacility(Point<G2D> location, FacilityType type, String name) {
         this.location = location;
         this.type = type;
         this.name = name;
+    }
+
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    private enum NotNullField{
+        LOCATION("경위도", FACILITY_LOCATION_REQUIRED),
+        TYPE("유형", FACILITY_TYPE_REQUIRED),
+        NAME("이름", FACILITY_NAME_REQUIRED),
+
+        ;
+
+        private final String description;
+        private final FacilityErrorStatus errorStatus;
     }
 
 }
