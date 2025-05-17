9044b5ce41baca2a6316d09bc5756a1502b6ee47
mungsil <thddmd2009@gmail.com>
2025-05-17 20:02:12 +0900

    feat: ReviewStat 팩토리 메서드에 도메인 제약 조건 작성 #3

--- src/main/java/org/songeun/petdongne_server/review/domain/error/ReviewErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/review/domain/error/ReviewErrorStatus.java
@@ -15,8 +15,10 @@ public enum ReviewErrorStatus implements ErrorStatus {
     REVIEW_RATING_RANGE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW_RATING_RANGE_INVALID", "평점은 0.0보다 크고 5.0 이하인 값만 가질 수 있습니다."),
     REVIEW_RATING_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_RATING_REQUIRED", "리뷰 평점은 NULL일 수 없습니다."),
 
+    REVIEW_AVERAGE_RATING_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_AVERAGE_RATING_REQUIRED", "리뷰 평균 평점은 NULL일 수 없습니다."),
+
     REVIEW_USER_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_USER_REQUIRED", "리뷰 작성자는 NULL일 수 없습니다."),
-    REVIEW_RESIDENTIAL_COMPLEX_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_RESIDENTIAL_COMPLEX_REQUIRED", "리뷰를 추가할 주거단지는 NULL일 수 없습니다."),
+    REVIEW_RESIDENTIAL_COMPLEX_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_RESIDENTIAL_COMPLEX_REQUIRED", "주거단지는 NULL일 수 없습니다."),
 
     LAST_OCCUPIED_YEAR_REQUIRED(HttpStatus.BAD_REQUEST, "LAST_OCCUPIED_YEAR_REQUIRED", "거주 연도는 NULL일 수 없습니다."),
     LAST_OCCUPIED_YEAR_INVALID(HttpStatus.BAD_REQUEST, "LAST_OCCUPIED_YEAR_INVALID", "거주 연도는 거주한 단지의 사용 승인일의 연도와 같거나 그 이후여야 합니다."),
@@ -24,7 +26,13 @@ public enum ReviewErrorStatus implements ErrorStatus {
     REVIEW_PHOTO_URL_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_PHOTO_URL_REQUIRED", "리뷰 사진의 URL은 NULL일 수 없습니다."),
     REVIEW_PHOTO_TYPE_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_PHOTO_TYPE_REQUIRED", "반려동물 여부는 NULL일 수 없습니다"),
 
-    REVIEW_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_REQUIRED", "리뷰는 NULL일 수 없습니다");
+    REVIEW_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_REQUIRED", "리뷰는 NULL일 수 없습니다"),
+
+    REVIEW_COUNT_INVALID(HttpStatus.BAD_REQUEST, "REVIEW_COUNT_INVALID", "리뷰 개수는 0개 이상이어야 합니다."),
+    REVIEW_COUNT_REQUIRED(HttpStatus.BAD_REQUEST, "REVIEW_COUNT_REQUIRED", "리뷰 개수는 NULL일 수 없습니다."),
+
+    ;
+
 
     private final HttpStatus httpStatus;
     private final String code;
