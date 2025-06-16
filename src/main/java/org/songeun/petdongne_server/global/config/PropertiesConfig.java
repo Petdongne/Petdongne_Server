0f9bd841e463fc72a241b2eb9d71fbb4afff323c
mungsil <thddmd2009@gmail.com>
2025-06-17 02:02:13 +0900

    feat: ConfigurationProperties 사용을 위한 설정 클래스 및 컴파일 옵션 추가 #7

---
+++ src/main/java/org/songeun/petdongne_server/global/config/PropertiesConfig.java
@@ -0,0 +1,10 @@
+package org.songeun.petdongne_server.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = {"org.songeun.petdongne_server"})
+public class PropertiesConfig {
+
+}
