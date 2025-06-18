7b57d02d6921b930d8c8f90052b89dbb4335c185
mungsil <thddmd2009@gmail.com>
2025-06-18 19:42:19 +0900

    feat: @Field 기반 인덱스 매핑 방법으로 수정 #7

--- src/main/java/org/songeun/petdongne_server/search/domain/document/Address.java
+++ src/main/java/org/songeun/petdongne_server/search/domain/document/Address.java
@@ -10,34 +10,41 @@ import org.springframework.data.elasticsearch.annotations.*;
 
 @Document(indexName = "kor-address-v1", createIndex = false, aliases = @Alias(value = "kor-address"))
 @Setting(settingPath = "/elasticsearch/address-setting.json")
-@Mapping(mappingPath = "/elasticsearch/address-mapping.json")
 @Getter
 @NoArgsConstructor
 public class Address {
 
     @Id
     @CsvBindByName(column = "코드")
+    @Field(type = FieldType.Keyword)
     private String id;
 
     @CsvBindByName(column = "시도명")
+    @Field(type = FieldType.Keyword)
     private String sido;
 
     @CsvBindByName(column = "시군구명")
+    @Field(type = FieldType.Keyword)
     private String sigungu;
 
     @CsvBindByName(column = "읍면동명")
+    @Field(type = FieldType.Keyword)
     private String eupmyeondong;
 
     @CsvBindByName(column = "리명")
+    @Field(type = FieldType.Keyword)
     private String re;
 
     @CsvBindByName(column = "주소")
+    @Field(type = FieldType.Text, analyzer = "address_search_analyzer")
     private String fullAddress;
 
     @CsvCustomBindByName(column = "계층", converter = AddressHierarchyConverter.class)
+    @Field(type = FieldType.Integer)
     private AddressHierarchy hierarchyLevel;
 
     @CsvCustomBindByName(column = "유형", converter = AddressTypeConverter.class)
+    @Field(type = FieldType.Keyword)
     private AddressType type;
 
 }
