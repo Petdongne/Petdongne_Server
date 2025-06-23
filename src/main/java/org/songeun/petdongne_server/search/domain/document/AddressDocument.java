211ade5c062b06d1212f25350e5c8e05ed3f1b36
mungsil <thddmd2009@gmail.com>
2025-06-23 19:08:55 +0900

    feat: 인덱스 필드명과 멤버 변수명을 명시적으로 분리하여 유지보수성 개선 #7

--- src/main/java/org/songeun/petdongne_server/search/domain/document/AddressDocument.java
+++ src/main/java/org/songeun/petdongne_server/search/domain/document/AddressDocument.java
@@ -21,23 +21,23 @@ public class Address {
 
     @Id
     @CsvBindByName(column = "코드")
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.ID)
     private String id;
 
     @CsvBindByName(column = "시도명")
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.SIDO)
     private String sido;
 
     @CsvBindByName(column = "시군구명")
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.SIGUNGU)
     private String sigungu;
 
     @CsvBindByName(column = "읍면동명")
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.EUPMYEONDONG)
     private String eupmyeondong;
 
     @CsvBindByName(column = "리명")
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.RE)
     private String re;
 
     @CsvBindByName(column = "주소")
@@ -45,11 +45,33 @@ public class Address {
     private String fullAddress;
 
     @CsvCustomBindByName(column = "계층", converter = AddressHierarchyConverter.class)
-    @Field(type = FieldType.Integer)
+    @Field(type = FieldType.Integer, name = AddressDocumentFields.HIERARCHY_LEVEL)
     private AddressHierarchy hierarchyLevel;
 
     @CsvCustomBindByName(column = "유형", converter = AddressTypeConverter.class)
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = AddressDocumentFields.TYPE)
     private AddressType type;
+
+    public static class AddressDocumentFields {
+        public static final String ID = "id";
+        public static final String SIDO = "sido";
+        public static final String SIGUNGU = "sigungu";
+        public static final String EUPMYEONDONG = "eupmyeondong";
+        public static final String RE = "re";
+        public static final String FULL_ADDRESS = "fullAddress";
+        private static final String FULL_ADDRESS_PER_CHAR_SUFFIX = "per_char";
+        private static final String FULL_ADDRESS_FIRST_CHAR_SUFFIX = "first_char";
+        public static final String HIERARCHY_LEVEL = "hierarchyLevel";
+        public static final String TYPE = "type";
+        public static final String SCORE = "_score";
+
+        public static String getFullAddressFirstChar(){
+            return FULL_ADDRESS + "." + FULL_ADDRESS_FIRST_CHAR_SUFFIX;
+        }
+
+        public static String getFullAddressPerChar(){
+            return FULL_ADDRESS + "." + FULL_ADDRESS_PER_CHAR_SUFFIX;
+        }
+    }
 
 }
