3dbe240eea6bb0398cfc9e41f98db3b0e82a0dcc
mungsil <thddmd2009@gmail.com>
2025-06-26 04:21:28 +0900

    feat: 공통 Page 응답 클래스에 정적 팩토리 메서드 작성 #7

--- src/main/java/org/songeun/petdongne_server/global/common/PagedResult.java
+++ src/main/java/org/songeun/petdongne_server/global/common/PagedResult.java
@@ -3,6 +3,8 @@ package org.songeun.petdongne_server.global.common;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.songeun.petdongne_server.addess.presentation.dto.AddressSearchResponseDto;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -17,5 +19,16 @@ public class PagedResult<T> {
     private final int totalPages;
     private final long totalItems;
     private final List<T> items;
+
+    public static <T> PagedResult<T> from(Page<T> page) {
+        return PagedResult.<T>builder()
+                .currentPage(page.getNumber())
+                .hasPrevious(page.hasPrevious())
+                .hasNext(page.hasNext())
+                .totalPages(page.getTotalPages())
+                .totalItems(page.getTotalElements())
+                .items(page.getContent())
+                .build();
+    }
 
 }
