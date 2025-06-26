9b76cdd7ccfb3e52beafb4c8cb5923b0344f14c6
mungsil <thddmd2009@gmail.com>
2025-06-26 16:52:57 +0900

    feat: 인덱스 존재 여부 체크 후 로깅 로직 추가 #7

---
+++ src/main/java/org/songeun/petdongne_server/addess/infrastructure/elasticsearch/config/ElasticsearchIndexValidator.java
@@ -0,0 +1,36 @@
+package org.songeun.petdongne_server.addess.infrastructure.elasticsearch.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.songeun.petdongne_server.addess.infrastructure.elasticsearch.document.AddressDocument;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@Profile("prod")
+@RequiredArgsConstructor
+public class ElasticsearchIndexValidator implements ApplicationRunner {
+
+    private final ElasticsearchOperations operations;
+
+    private final List<Class<?>> requiredIndices = List.of(
+            AddressDocument.class
+    );
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // TODO: 운영 환경 배포 전, 안정성을 위해 로깅 대신 예외를 발생시켜 기동을 실패시켜야 함
+        requiredIndices.stream()
+                .filter(idx -> !operations.indexOps(idx).exists())
+                .forEach(notExistIdx ->
+                        log.warn("Required index '{}' does not exist in Elasticsearch", notExistIdx.getSimpleName())
+                );
+    }
+
+}
