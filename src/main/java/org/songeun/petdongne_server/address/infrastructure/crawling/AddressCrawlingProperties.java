ef1e04d450f55d9471448c88bf0d48851418f959
mungsil <thddmd2009@gmail.com>
2025-07-17 23:51:32 +0900

    feat: 주소 크롤링 설정용 환경변수 클래스 추가 #8

---
+++ src/main/java/org/songeun/petdongne_server/address/infrastructure/crawling/AddressCrawlingProperties.java
@@ -0,0 +1,32 @@
+package org.songeun.petdongne_server.address.infrastructure.crawling;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Valid
+@ConfigurationProperties("app.crawling.address")
+public class AddressCrawlingProperties {
+
+    @NotBlank
+    private final String postListBaseUrl;
+
+    @NotBlank
+    private final String postBaseUrl;
+
+    @NotBlank
+    private final String userAgent;
+
+    @NotBlank
+    private final String outputFileSavePath;
+
+    public AddressCrawlingProperties(String postListBaseUrl, String postBaseUrl, String userAgent, String outputFileSavePath) {
+        this.postListBaseUrl = postListBaseUrl;
+        this.postBaseUrl = postBaseUrl;
+        this.userAgent = userAgent;
+        this.outputFileSavePath = outputFileSavePath;
+    }
+
+}
