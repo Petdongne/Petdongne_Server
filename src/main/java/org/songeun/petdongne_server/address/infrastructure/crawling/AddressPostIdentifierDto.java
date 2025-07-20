a0a04b66ca232b41609c15f615deb5526ed24aa9
mungsil <thddmd2009@gmail.com>
2025-07-20 23:18:41 +0900

    feat: 주소 데이터 크롤러 구현 #8

---
+++ src/main/java/org/songeun/petdongne_server/address/infrastructure/crawling/AddressPostIdentifierDto.java
@@ -0,0 +1,17 @@
+package org.songeun.petdongne_server.address.infrastructure.crawling;
+
+/**
+ * 주소 데이터 파일의 식별 정보를 담는 클래스입니다.
+ * @param bbsId 주소 파일이 있는 게시글의 식별을 위한 값
+ * @param nttId 주소 파일이 있는 게시글의 식별을 위한 값
+ */
+public record AddressPostIdentifierDto(
+        String bbsId,
+        Long nttId
+) {
+
+    public static AddressPostIdentifierDto of(final String bbsId, final Long nttId) {
+        return new AddressPostIdentifierDto(bbsId, nttId);
+    }
+
+}
