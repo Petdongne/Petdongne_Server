3a3e6e34a6d873728de92062a74e8b4d5e47f5e5
mungsil <thddmd2009@gmail.com>
2025-08-01 00:28:03 +0900

    refactor: 구체적 -> 추상적인 설명으로 수정 #8

--- src/test/java/org/songeun/petdongne_server/address/infrastructure/crawling/history/CrawledAddressPostRepositoryTest.java
+++ src/test/java/org/songeun/petdongne_server/address/infrastructure/crawling/history/CrawledAddressPostRepositoryTest.java
@@ -2,7 +2,7 @@ package org.songeun.petdongne_server.address.infrastructure.crawling.history;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.songeun.petdongne_server.address.support.IntegrationTestSupport;
+import org.songeun.petdongne_server.testSupport.IntegrationTestSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -17,8 +17,8 @@ class CrawledAddressPostRepositoryTest extends IntegrationTestSupport {
     private CrawledAddressPostRepository repository;
     
     @Test
-    @DisplayName("최신 게시글(NttId 기준)을 조회한다") // NttId 값이 클수록 최신
-    void shouldReturnLargestNttId(){
+    @DisplayName("가장 최근에 크롤링한 게시글을 조회한다.") 
+    void shouldReturnLargestIdentifier(){
         //given
         var crawled = createCrawledAddressPosts(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L);
         repository.saveAll(crawled);
