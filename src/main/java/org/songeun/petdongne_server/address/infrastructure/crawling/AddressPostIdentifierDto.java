07a0a25db7a560b0a436ce7d8e8c00d3c9feb186
mungsil <thddmd2009@gmail.com>
2025-08-02 01:06:01 +0900

    feat: DTO 생성 시 필수값 검증 로직 추가 #8

--- src/main/java/org/songeun/petdongne_server/address/infrastructure/crawling/AddressPostIdentifierDto.java
+++ src/main/java/org/songeun/petdongne_server/address/infrastructure/crawling/AddressPostIdentifierDto.java
@@ -1,5 +1,9 @@
 package org.songeun.petdongne_server.address.infrastructure.crawling;
 
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
 /**
  * 주소 데이터 파일의 식별 정보를 담는 클래스입니다.
  * @param bbsId 주소 파일이 있는 게시글의 식별을 위한 값
@@ -11,6 +15,9 @@ public record AddressPostIdentifierDto(
 ) {
 
     public static AddressPostIdentifierDto of(final String bbsId, final Long nttId) {
+        StringUtils.hasText(bbsId);
+        Objects.requireNonNull(nttId);
+
         return new AddressPostIdentifierDto(bbsId, nttId);
     }
 
