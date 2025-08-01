e5486f406f744907c9b985c2b9b1bed5df842b5b
mungsil <thddmd2009@gmail.com>
2025-08-01 19:49:28 +0900

    feat: 인덱스 생성 실패 시 false를 반환하도록 수정 #8

--- src/main/java/org/songeun/petdongne_server/global/elasticsearch/ElasticsearchIndexOperationsSupport.java
+++ src/main/java/org/songeun/petdongne_server/global/elasticsearch/ElasticsearchIndexOperationsSupport.java
@@ -1,6 +1,7 @@
 package org.songeun.petdongne_server.global.elasticsearch;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.document.Document;
@@ -8,6 +9,7 @@ import org.springframework.data.elasticsearch.core.index.Settings;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ElasticsearchIndexOperationsSupport {
@@ -15,11 +17,16 @@ public class ElasticsearchIndexOperationsSupport {
     private final ElasticsearchOperations operations;
 
     public boolean createIndex(IndexCoordinates indexCoordinates, Class<?> clazz) {
-        IndexOperations indexOperations = operations.indexOps(indexCoordinates);
-        Document mapping = indexOperations.createMapping(clazz);
-        Settings settings = indexOperations.createSettings(clazz);
+        try {
+            IndexOperations indexOperations = operations.indexOps(indexCoordinates);
+            Document mapping = indexOperations.createMapping(clazz);
+            Settings settings = indexOperations.createSettings(clazz);
 
-        return indexOperations.create(settings, mapping);
+            return indexOperations.create(settings, mapping);
+        } catch (Exception e) {
+             log.warn("Failed to create index {}: {}", indexCoordinates.getIndexName(), e.getMessage());
+            return false;
+        }
     }
 
     public boolean deleteIndex(IndexCoordinates indexCoords) {
