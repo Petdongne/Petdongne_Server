02d5eef14313f8476249cb85583d28f581e69dfb
mungsil <thddmd2009@gmail.com>
2025-09-16 04:14:44 +0900

    feat: 에러 코드 작성 #12

--- src/main/java/org/songeun/petdongne_server/global/common/GlobalErrorStatus.java
+++ src/main/java/org/songeun/petdongne_server/global/common/GlobalErrorStatus.java
@@ -20,6 +20,9 @@ public enum GlobalErrorStatus implements ErrorStatus {
     NOT_FOUND(HttpStatus.NOT_FOUND,"NOT_FOUND", "요청하신 리소스를 찾을 수 없습니다."),
     CONFLICT(HttpStatus.CONFLICT, "CONFLICT", "이미 존재하는 리소스입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버에 오류가 발생했습니다."),
+
+    NULL_OBJECT_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "NULL_OBJECT_NOT_ALLOWED", "null일 수 없습니다."),
+    NULL_OR_EMPTY_STRING_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "NULL_OR_EMPTY_STRING_NOT_ALLOWED", "null이거나 빈 문자열일 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
