e3ebc1694414a3d577942e9b0a46918f2df6a9eb
mungsil <thddmd2009@gmail.com>
2025-09-16 05:06:24 +0900

    fix: postgis 이미지 사용을 위한 변경  #12

--- src/test/java/org/songeun/petdongne_server/testSupport/PostgresSQLIntegrationTestSupport.java
+++ src/test/java/org/songeun/petdongne_server/testSupport/PostgresSQLIntegrationTestSupport.java
@@ -5,12 +5,16 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public class PostgresSQLIntegrationTestSupport extends IntegrationTestSupport {
 
+    static DockerImageName postgis = DockerImageName.parse("postgis/postgis:16-3.4-alpine")
+            .asCompatibleSubstituteFor("postgres");
+
     @Container
-    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgis/postgis:16-3.4-alpine")
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(postgis)
             .withDatabaseName("testdb")
             .withUsername("testuser")
             .withPassword("testpass")
