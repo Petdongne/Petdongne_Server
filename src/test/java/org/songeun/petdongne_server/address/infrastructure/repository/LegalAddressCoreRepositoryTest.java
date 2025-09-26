fee9bd2da8675fbdb7be26dfc941c5f6ac3fec55
mungsil <thddmd2009@gmail.com>
2025-09-26 23:08:11 +0900

    fix: Postgis 함수 사용으로 인해 테스트 컨테이너 상속으로 변경 #18

--- src/test/java/org/songeun/petdongne_server/address/infrastructure/repository/LegalAddressCoreRepositoryTest.java
+++ src/test/java/org/songeun/petdongne_server/address/infrastructure/repository/LegalAddressCoreRepositoryTest.java
@@ -6,13 +6,14 @@ import org.songeun.petdongne_server.address.domain.LegalAddress;
 import org.songeun.petdongne_server.address.domain.RegionAddressLevel;
 import org.songeun.petdongne_server.address.fixture.LegalAddressFixture;
 import org.songeun.petdongne_server.testSupport.IntegrationTestSupport;
+import org.songeun.petdongne_server.testSupport.PostgresSQLIntegrationTestSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LegalAddressCoreRepositoryTest extends IntegrationTestSupport {
+class LegalAddressCoreRepositoryTest extends PostgresSQLIntegrationTestSupport {
 
     @Autowired
     private LegalAddressCoreRepository legalAddressCoreRepository;
