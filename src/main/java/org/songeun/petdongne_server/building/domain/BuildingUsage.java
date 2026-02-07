c85743220138d3ad71965574f94c9b537b344236
mungsil <thddmd2009@gmail.com>
2026-02-07 17:24:04 +0900

    feat: 데이터 소스 변경으로 인한 Building Table 설계 수정
    - Building Table의 데이터는 외부 제공 데이터를 기반으로 구축됩니다.
    - 기존 데이터 소스를 지속적으로 구축 및 관리하기에 어려움이 있어, '부동산 빅데이터 플랫폼'에서 제공하는 데이터를 활용합니다.
    - 새로운 데이터의 스키마에 적합하게 Building 테이블을 재설계하였습니다.
    
    Related-issues: #19

--- src/main/java/org/songeun/petdongne_server/building/domain/BuildingUsage.java
+++ src/main/java/org/songeun/petdongne_server/building/domain/BuildingUsage.java
@@ -10,23 +10,9 @@ import java.util.Arrays;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum BuildingUsage {
 
-    SINGLE_HOUSE_1("단독주택", "01000"),
-    SINGLE_HOUSE_2("단독주택", "01001"),
-    MULTI_HOUSE("다중주택", "01002"),
-    MULTI_FAMILY_HOUSE("다가구 주택", "01003"),
-    APARTMENT_COMPLEX("공동주택", "02000"),
-    APARTMENT("아파트", "02001"),
-    TOWNHOUSE("연립주택", "02002"),
-    MULTI_UNIT_HOUSE("다세대 주택", "02003"),
-    CONVENIENCE_FACILITY("생활편익시설", "02004"),
-    NEIGHBORHOOD_FACILITY_TYPE1("제1종근린생활시설", "03000"),
-    OTHER_NEIGHBORHOOD_FACILITY_TYPE1("기타제1종 근린생활시설", "03999"),
-    NEIGHBORHOOD_FACILITY_TYPE2("제2종근린생활시설", "04000"),
-    VETERINARY_HOSPITAL("동물병원", "04012"),
-    OTHER_NEIGHBORHOOD_FACILITY_TYPE2("기타제2종 근린생활시설", "04999"),
-    OFFICETEL("오피스텔", "10202"),
-    OFFICE_FACILITY("업무시설", "10000"),
-    OTHER_GENERAL_OFFICE_FACILITY("기타일반업무시설", "10299");
+    APARTMENT("아파트", "1"),
+    TOWNHOUSE("연립주택", "2"),
+    MULTI_UNIT_HOUSE("다세대 주택", "3");
 
     private final String value;
     private final String code;
@@ -36,7 +22,7 @@ public enum BuildingUsage {
         return Arrays.stream(BuildingUsage.values())
                 .filter(t-> t.code.equals(code))
                 .findAny()
-                .orElseThrow(()-> new RuntimeException("엥? 우리 그런애 없어요;;"));
+                .orElseThrow(()-> new RuntimeException("존재하지 않는 코드입니다. unknown code: " + code));
     }
 
 }
