73aefdcb97d480d4c07b79fe5ddf958fab6057e7
mungsil <thddmd2009@gmail.com>
2026-02-24 17:52:07 +0900

    fix: 누락된 인증 실패 핸들러 추가 #34

--- src/main/java/org/songeun/petdongne_server/security/authentication/OpaqueTokenAuthenticationFilter.java
+++ src/main/java/org/songeun/petdongne_server/security/authentication/OpaqueTokenAuthenticationFilter.java
@@ -3,6 +3,7 @@ package org.songeun.petdongne_server.security.authentication;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,8 +15,10 @@ public class OpaqueTokenAuthenticationFilter extends AuthenticationFilter {
             AuthenticationManager opaqueTokenAuthenticationManager,
             AuthenticationConverter opaqueTokenAuthenticationConverter,
             @Qualifier("opaqueTokenAuthenticationSuccessHandler")
-            AuthenticationSuccessHandler opaqueTokenAuthenticationSuccessHandler) {
+            AuthenticationSuccessHandler opaqueTokenAuthenticationSuccessHandler,
+            AuthenticationFailureHandler opaqueTokenAuthenticationFailureHandler) {
         super(opaqueTokenAuthenticationManager, opaqueTokenAuthenticationConverter);
         setSuccessHandler(opaqueTokenAuthenticationSuccessHandler);
+        setFailureHandler(opaqueTokenAuthenticationFailureHandler);
     }
 }
